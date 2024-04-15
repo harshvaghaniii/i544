988fd670c41a394c1316a7e70b474ca94b740e76
Harsh Vaghani <hvaghani@binghamton.edu>
2024-04-15 19:28:26 -0400

    implementing findBooksByUrl

--- submit/prj4-sol/src/lib/app.ts
+++ submit/prj4-sol/src/lib/app.ts
@@ -1,59 +1,78 @@
-import { Errors } from 'cs544-js-utils';
+import { Errors } from "cs544-js-utils";
 
 //types defined in library.ts in earlier projects
-import * as Lib from 'library-types';
+import * as Lib from "library-types";
 
+import {
+	NavLinks,
+	LinkedResult,
+	PagedEnvelope,
+	SuccessEnvelope,
+} from "./response-envelopes.js";
 
-import { NavLinks, LinkedResult, PagedEnvelope, SuccessEnvelope }
-  from './response-envelopes.js';
+import { makeLibraryWs, LibraryWs } from "./library-ws.js";
 
-import { makeLibraryWs, LibraryWs } from './library-ws.js';
-
-import { makeElement, makeQueryUrl } from './utils.js';
+import { makeElement, makeQueryUrl } from "./utils.js";
 
 export default function makeApp(wsUrl: string) {
-  return new App(wsUrl);
+	return new App(wsUrl);
 }
 
-
 class App {
-  private readonly wsUrl: string;
-  private readonly ws: LibraryWs;
+	private readonly wsUrl: string;
+	private readonly ws: LibraryWs;
 
-  private readonly result: HTMLElement;
-  private readonly errors: HTMLElement;
+	private readonly result: HTMLElement;
+	private readonly errors: HTMLElement;
 
-  constructor(wsUrl: string) {
-    this.wsUrl = wsUrl;
-    this.ws = makeLibraryWs(wsUrl);
-    this.result = document.querySelector('#result');
-    this.errors = document.querySelector('#errors');
-    //TODO: add search handler
-  }
-  
-  //TODO: add private methods as needed
+	constructor(wsUrl: string) {
+		this.wsUrl = wsUrl;
+		this.ws = makeLibraryWs(wsUrl);
+		this.result = document.querySelector("#result");
+		this.errors = document.querySelector("#errors");
+		//TODO: add search handler
+		this.searchHandler = this.searchHandler.bind(this);
+		const form = document.querySelector("form.grid-form");
+		if (form) {
+			form.addEventListener("submit", this.searchHandler);
+		}
+	}
+	private async searchHandler(event: Event) {
+		event.preventDefault();
+		const searchField: HTMLInputElement | null = document.getElementById(
+			"search"
+		) as HTMLInputElement;
+		const userSearch: string = searchField.value;
+		const baseURL = this.wsUrl + "/api/books";
+		const findUrl = makeQueryUrl(baseURL, {
+			search: userSearch,
+		});
+		const searchObj = new LibraryWs(baseURL);
+		const res = await searchObj.findBooksByUrl(findUrl);
+		console.log(JSON.stringify(res));
+		// Further actions related to search handling can be added here
+	}
+	//TODO: add private methods as needed
 
-  /** unwrap a result, displaying errors if !result.isOk, 
-   *  returning T otherwise.   Use as if (unwrap(result)) { ... }
-   *  when T !== void.
-   */
-  private unwrap<T>(result: Errors.Result<T>) {
-    if (result.isOk === false) {
-      displayErrors(result.errors);
-    }
-    else {
-      return result.val;
-    }
-  }
+	/** unwrap a result, displaying errors if !result.isOk,
+	 *  returning T otherwise.   Use as if (unwrap(result)) { ... }
+	 *  when T !== void.
+	 */
+	private unwrap<T>(result: Errors.Result<T>) {
+		if (result.isOk === false) {
+			displayErrors(result.errors);
+		} else {
+			return result.val;
+		}
+	}
 
-  /** clear out all errors */
-  private clearErrors() {
-    this.errors.innerHTML = '';
-    document.querySelectorAll(`.error`).forEach( el => {
-      el.innerHTML = '';
-    });
-  }
-
+	/** clear out all errors */
+	private clearErrors() {
+		this.errors.innerHTML = "";
+		document.querySelectorAll(`.error`).forEach((el) => {
+			el.innerHTML = "";
+		});
+	}
 } //class App
 
 /** Display errors. If an error has a widget or path widgetId such
@@ -61,19 +80,18 @@ class App {
  *  then the error message is added to that element; otherwise the
  *  error message is added to the element having to the element having
  *  ID `errors` wrapped within an `<li>`.
- */  
+ */
 function displayErrors(errors: Errors.Err[]) {
-  for (const err of errors) {
-    const id = err.options.widget ?? err.options.path;
-    const widget = id && document.querySelector(`#${id}-error`);
-    if (widget) {
-      widget.append(err.message);
-    }
-    else {
-      const li = makeElement('li', {class: 'error'}, err.message);
-      document.querySelector(`#errors`)!.append(li);
-    }
-  }
+	for (const err of errors) {
+		const id = err.options.widget ?? err.options.path;
+		const widget = id && document.querySelector(`#${id}-error`);
+		if (widget) {
+			widget.append(err.message);
+		} else {
+			const li = makeElement("li", { class: "error" }, err.message);
+			document.querySelector(`#errors`)!.append(li);
+		}
+	}
 }
 
 //TODO: add functions as needed
