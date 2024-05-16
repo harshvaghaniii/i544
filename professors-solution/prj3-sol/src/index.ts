6918ee2cef54eb0156bfa9daaea78f34c6d8d943
Harsh Vaghani <hvaghani@binghamton.edu>
2024-05-16 10:52:03 -0400

    deleting the solutions

--- professors-solution/prj3-sol/src/index.ts
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-
-import { main } from './lib/main.js';
-
-main(process.argv.slice(2)).catch((err: any) => console.error(err));
--- professors-solution/prj3-sol/src/lib/params.ts
+++
@@ -1,2 +0,0 @@
-export const DEFAULT_INDEX = 0;
-export const DEFAULT_COUNT = 5;
